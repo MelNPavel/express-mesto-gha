bad8e03d7b7434d259bdaed293e7d4856c3382af
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-15 22:15:00 +0300

    feat: Сдача на проверку

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,13 @@ const {
   getUserMe,
 } = require('../controllers/users');
 
-usersRouters.get('/users', express.json(), getUsers);
-usersRouters.get('/users/me', express.json(), getUserMe);
-usersRouters.get('/users/:userId', express.json(), userFindId);
+usersRouters.get('/users', getUsers);
+usersRouters.get('/users/me', getUserMe);
+usersRouters.get('/users/:userId', userFindId);
 
-usersRouters.patch('/users/me', express.json(), userUpdate);
-usersRouters.patch('/users/me/avatar', express.json(), avatarUpdate);
+usersRouters.patch('/users/me', userUpdate);
+usersRouters.patch('/users/me/avatar', avatarUpdate);
 
 module.exports = { usersRouters };
+
+// express.json(),
