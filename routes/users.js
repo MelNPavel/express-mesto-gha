43c919a1a5af432dbea9606f2a054122a4f7fa43
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-16 14:52:08 +0300

    feat: Сдача на проверку

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { celebrate, Joi } = require('celebrate');
+
 const usersRouters = express.Router();
 const {
   getUsers,
@@ -13,7 +15,21 @@ usersRouters.get('/users', express.json(), getUsers);
 usersRouters.get('/users/me', express.json(), getUserMe);
 usersRouters.get('/users/:userId', express.json(), userFindId);
 
-usersRouters.patch('/users/me', express.json(), userUpdate);
-usersRouters.patch('/users/me/avatar', express.json(), avatarUpdate);
+usersRouters.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
+    about: Joi.string().min(2).max(30).default('Исследователь'),
+  }),
+}), userUpdate);
+
+usersRouters.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi
+      .string()
+      .default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png')
+      // eslint-disable-next-line no-useless-escape
+      .regex(/((?:(?:http?)[s]*:\/\/)?[a-z0-9-%\/\&=?\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?)/),
+  }),
+}), avatarUpdate);
 
 module.exports = { usersRouters };
