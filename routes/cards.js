4e0b77cae45ac252404f88e2373d1f516d700066
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-16 16:08:00 +0300

    feat: Сдача на проверку

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 
 const cardsRouters = express.Router();
 const {
@@ -13,5 +14,9 @@ cardsRouters.get('/cards', express.json(), getCard);
 cardsRouters.post('/cards', express.json(), createCard);
 cardsRouters.delete('/cards/:_id', express.json(), deleteCard);
 cardsRouters.put('/cards/:cardId/likes', express.json(), likeCard);
-cardsRouters.delete('/cards/:cardId/likes', express.json(), dislikeCard);
+cardsRouters.delete('/cards/:cardId/likes', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string(),
+  }),
+}), dislikeCard);
 module.exports = { cardsRouters };
