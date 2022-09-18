c968ed2e8486d5eba24dd30e6d15c47a6a4470a6
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-18 16:04:51 +0300

    feat: Сдача на проверку

--- controllers/users.js
+++ controllers/users.js
@@ -22,7 +22,7 @@ const getUsers = async (req, res) => {
 const getUserMe = async (req, res, next) => {
   const userId = req.user._id;
   try {
-    const user = await User.findById({ userId });
+    const user = await User.findById(userId);
     if (!user) {
       return next(new NotFoudError('Пользователь по указанному _id не найден.'));
     }
@@ -38,7 +38,7 @@ const getUserMe = async (req, res, next) => {
 const userFindId = async (req, res, next) => {
   try {
     const { userId } = req.params;
-    const user = await User.findById({ userId });
+    const user = await User.findById(userId);
     if (!user) {
       return next(new NotFoudError('Пользователь по указанному _id не найден.'));
     }
