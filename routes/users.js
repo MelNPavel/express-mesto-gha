a42b0613b3f8d1727384efd92484fcfb89a12519
Pavel Melnikov <PavelNMel@yandex.ru>
2022-09-18 22:26:52 +0300

    feat: Сдача на проверку

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ usersRouters.get('/users/me', getUserMe);
 
 usersRouters.get('/users/:userId', celebrate({
   body: Joi.object().keys({
-    userId: Joi.string(),
+    userId: Joi.string().alphanum().length(24),
   }),
 }), userFindId);
 
